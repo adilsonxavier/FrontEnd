8619de928dcae0cb69e5d149d1be87921c60a60a
adilsonxavier <adilsonxdesouza@gmail.com>
2021-12-09 00:02:28 -0300

    primeiro commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+﻿import React from "react";
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <React.StrictMode>
+        {/* <App />*/}
+        <h2>Vim do index.js 2327</h2>
+    </React.StrictMode>,
+    // document.getElementById('app')
+    document.querySelector("#app")
+);
+
